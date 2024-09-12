150a2009b4ff79cb92a89634e586a103f0c0236a
harisreedhar <h4harisreedhar.s.s@gmail.com>
2024-09-12 20:42:25 +0530

    changes

--- arcface_converter/model.py
+++ arcface_converter/model.py
@@ -3,17 +3,19 @@ import torch.nn as nn
 
 
 class ArcFaceConverter(nn.Module):
-    def __init__(self, input_dim : int = 512, output_dim : int = 512) -> None:
-        super(ArcFaceConverter, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, 1024)
-        self.fc4 = nn.Linear(1024, output_dim)
-        self.activation = nn.LeakyReLU()
+	def __init__(self) -> None:
+		super(ArcFaceConverter, self).__init__()
+		input_dim = 512
+		output_dim = 512
+		self.fc1 = nn.Linear(input_dim, 1024)
+		self.fc2 = nn.Linear(1024, 1024)
+		self.fc3 = nn.Linear(1024, 1024)
+		self.fc4 = nn.Linear(1024, output_dim)
+		self.activation = nn.LeakyReLU()
 
-    def forward(self, input_embedding: torch.Tensor) -> torch.Tensor:
-        output_embedding = self.activation(self.fc1(input_embedding))
-        output_embedding = self.activation(self.fc2(output_embedding))
-        output_embedding = self.activation(self.fc3(output_embedding))
-        output_embedding = self.fc4(output_embedding)
-        return output_embedding
+	def forward(self, input_embedding: torch.Tensor) -> torch.Tensor:
+		output_embedding = self.activation(self.fc1(input_embedding))
+		output_embedding = self.activation(self.fc2(output_embedding))
+		output_embedding = self.activation(self.fc3(output_embedding))
+		output_embedding = self.fc4(output_embedding)
+		return output_embedding
--- arcface_converter/train.py
+++ arcface_converter/train.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import configparser
-from typing import Any
+from typing import Any, Tuple
 
 import numpy
-import pytorch_lightning as pl
+import pytorch_lightning
 import torch
 from model import ArcFaceConverter
+from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.tuner.tuning import Tuner
@@ -16,7 +17,7 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read('config.ini')
 
 
-class ArcFaceConverterTrainer(pl.LightningModule):
+class ArcFaceConverterTrainer(pytorch_lightning.LightningModule):
     def __init__(self) -> None:
         super(ArcFaceConverterTrainer, self).__init__()
         self.model = ArcFaceConverter()
@@ -26,26 +27,28 @@ class ArcFaceConverterTrainer(pl.LightningModule):
     def forward(self, input_embedding: torch.Tensor) -> torch.Tensor:
         return self.model(input_embedding)
 
-    def training_step(self, batch : Any, batch_idx : int) -> Any:
+    def training_step(self, batch : Tuple[torch.Tensor, torch.Tensor], batch_index : int) -> torch.Tensor:
         source, target = batch
         output = self(source)
         loss = self.loss_fn(output, target)
-        self.log('train_loss', loss, prog_bar=True, logger=True)
+        self.log('train_loss', loss, prog_bar = True, logger = True)
         return loss
 
-    def validation_step(self, batch : Any, batch_idx : int) -> Any:
+    def validation_step(self, batch : Tuple[torch.Tensor, torch.Tensor], batch_index : int) -> torch.Tensor:
         source, target = batch
         output = self(source)
         loss = self.loss_fn(output, target)
-        self.log('val_loss', loss, prog_bar=True, logger=True)
+        self.log('val_loss', loss, prog_bar = True, logger = True)
         return loss
 
     def configure_optimizers(self) -> Any:
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, factor=0.1, mode='min')
-        return {
+        optimizer = torch.optim.Adam(self.parameters(), lr = self.lr)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience = 10, factor = 0.1, mode = 'min')
+        return\
+		{
             'optimizer': optimizer,
-            'lr_scheduler': {
+            'lr_scheduler':
+			{
                 'scheduler': scheduler,
                 'monitor': 'train_loss',
                 'interval': 'epoch',
@@ -54,19 +57,19 @@ class ArcFaceConverterTrainer(pl.LightningModule):
         }
 
 
-def get_data_loader(batch_size : int, split_ratio : float = 0.8) -> Any:
+def get_data_loader(batch_size : int, split_ratio : float = 0.8) -> Tuple[DataLoader[Tuple[torch.Tensor, ...]], DataLoader[Tuple[torch.Tensor, ...]]]:
 	source = torch.from_numpy(numpy.load(CONFIG['embeddings']['source_path'])).float()
 	target = torch.from_numpy(numpy.load(CONFIG['embeddings']['target_path'])).float()
 	dataset = TensorDataset(source, target)
 	train_size = int(split_ratio * len(dataset))
-	val_size = len(dataset) - train_size
-	train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-	val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
-	return train_loader, val_loader
+	validation_size = len(dataset) - train_size
+	train_dataset, validation_dataset = random_split(dataset, [ train_size, validation_size ])
+	train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True, num_workers = 8, pin_memory = True)
+	validation_loader = DataLoader(validation_dataset, batch_size = batch_size, shuffle = False, num_workers = 8, pin_memory = True)
+	return train_loader, validation_loader
 
 
-def train(trainer : Any, train_loader : Any, val_loader : Any) -> None:
+def train(trainer : Trainer, train_loader : DataLoader[Tuple[torch.Tensor, ...]], val_loader : DataLoader[Tuple[torch.Tensor, ...]]) -> None:
 	model = ArcFaceConverterTrainer()
 	tuner = Tuner(trainer)
 	tuner.lr_find(model, train_loader, val_loader)
@@ -75,26 +78,26 @@ def train(trainer : Any, train_loader : Any, val_loader : Any) -> None:
 
 if __name__ == '__main__':
 	accelerator = 'gpu' if torch.cuda.is_available() else 'cpu'
-	device = 1 if torch.cuda.is_available() else None
+	devices = [ 1 ] if torch.cuda.is_available() else None
 	batch_size = 50000
-	train_loader, val_loader = get_data_loader(batch_size)
+	train_loader, validation_loader = get_data_loader(batch_size)
 
 	checkpoint_callback = ModelCheckpoint(
-		monitor="train_loss",
-		dirpath=CONFIG['checkpoints']['save_path'],
-		filename=CONFIG['checkpoints']['save_name'] + '-{epoch:02d}-{val_loss:.4f}',
-		save_top_k=3,
-		mode="min",
-		every_n_epochs=10
+		monitor = "train_loss",
+		dirpath = CONFIG['checkpoints']['directory_path'],
+		filename = CONFIG['checkpoints']['file_name'],
+		save_top_k = 3,
+		mode = "min",
+		every_n_epochs = 10
 	)
 
-	trainer = pl.Trainer(
-		max_epochs=1000,
-		devices=device,
-		accelerator=accelerator,
-		callbacks=[checkpoint_callback],
-		enable_progress_bar=True,
-		log_every_n_steps=2
+	trainer = Trainer(
+		max_epochs = 1000,
+		devices = devices,
+		accelerator = accelerator,
+		callbacks = [ checkpoint_callback ],
+		enable_progress_bar = True,
+		log_every_n_steps = 2
 	)
-	logger = TensorBoardLogger('logs/', name='arcface_converter')
-	train(trainer, train_loader, val_loader)
+	logger = TensorBoardLogger('logs/', name = 'arcface_converter')
+	train(trainer, train_loader, validation_loader)
