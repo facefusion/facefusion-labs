c08c293e6d66d5312c33d777ab38e6a9b2dcb03c
henryruhs <info@henryruhs.com>
2025-01-14 12:55:48 +0100

    Fix indent to tabs

--- face_swapper/src/discriminator.py
+++ face_swapper/src/discriminator.py
@@ -7,70 +7,70 @@ from .typing import DiscriminatorOutputs, Tensor
 
 
 class NLayerDiscriminator(nn.Module):
-    def __init__(self, input_channels : int, num_filters : int, num_layers : int) -> None:
-        super(NLayerDiscriminator, self).__init__()
-        self.num_layers = num_layers
-        kernel_size = 4
-        padding_size = int(numpy.ceil((kernel_size - 1.0) / 2))
-        model_layers = [
+	def __init__(self, input_channels : int, num_filters : int, num_layers : int) -> None:
+		super(NLayerDiscriminator, self).__init__()
+		self.num_layers = num_layers
+		kernel_size = 4
+		padding_size = int(numpy.ceil((kernel_size - 1.0) / 2))
+		model_layers = [
 			[
 				nn.Conv2d(input_channels, num_filters, kernel_size = kernel_size, stride = 2, padding = padding_size),
 			 	nn.LeakyReLU(0.2, True)
 			]]
-        current_filters = num_filters
+		current_filters = num_filters
 
-        for layer_index in range(1, num_layers):
-            previous_filters = current_filters
-            current_filters = min(current_filters * 2, 512)
-            model_layers += [
+		for layer_index in range(1, num_layers):
+			previous_filters = current_filters
+			current_filters = min(current_filters * 2, 512)
+			model_layers += [
 				[
-                	nn.Conv2d(previous_filters, current_filters, kernel_size = kernel_size, stride = 2, padding = padding_size),
-                	nn.InstanceNorm2d(current_filters), nn.LeakyReLU(0.2, True)
-        		]]
-        previous_filters = current_filters
-        current_filters = min(current_filters * 2, 512)
-        model_layers += [
+					nn.Conv2d(previous_filters, current_filters, kernel_size = kernel_size, stride = 2, padding = padding_size),
+					nn.InstanceNorm2d(current_filters), nn.LeakyReLU(0.2, True)
+				]]
+		previous_filters = current_filters
+		current_filters = min(current_filters * 2, 512)
+		model_layers += [
 			[
-            	nn.Conv2d(previous_filters, current_filters, kernel_size = kernel_size, stride = 1, padding = padding_size),
-            	nn.InstanceNorm2d(current_filters),
-            	nn.LeakyReLU(0.2, True)
-        	]]
-        model_layers += [
+				nn.Conv2d(previous_filters, current_filters, kernel_size = kernel_size, stride = 1, padding = padding_size),
+				nn.InstanceNorm2d(current_filters),
+				nn.LeakyReLU(0.2, True)
+			]]
+		model_layers += [
 			[
 				nn.Conv2d(current_filters, 1, kernel_size = kernel_size, stride = 1, padding = padding_size)
 			]]
-        combined_layers = []
+		combined_layers = []
 
-        for layer in model_layers:
-            combined_layers += layer
-        self.model = nn.Sequential(*combined_layers)
+		for layer in model_layers:
+			combined_layers += layer
+		self.model = nn.Sequential(*combined_layers)
 
-    def forward(self, input_tensor : Tensor) -> Tensor:
-        return self.model(input_tensor)
+	def forward(self, input_tensor : Tensor) -> Tensor:
+		return self.model(input_tensor)
 
 
 class MultiscaleDiscriminator(nn.Module):
-    def __init__(self, input_channels : int, num_filters : int, num_layers : int, num_discriminators : int):
-        super(MultiscaleDiscriminator, self).__init__()
-        self.num_discriminators = num_discriminators
-        self.num_layers = num_layers
+	def __init__(self, input_channels : int, num_filters : int, num_layers : int, num_discriminators : int):
+		super(MultiscaleDiscriminator, self).__init__()
+		self.num_discriminators = num_discriminators
+		self.num_layers = num_layers
 
-        for discriminator_index in range(num_discriminators):
-            single_discriminator = NLayerDiscriminator(input_channels, num_filters, num_layers)
-            setattr(self, 'discriminator_layer_{}'.format(discriminator_index), single_discriminator.model)
-        self.downsample = nn.AvgPool2d(kernel_size = 3, stride = 2, padding = [ 1, 1 ], count_include_pad = False) # type:ignore[arg-type]
+		for discriminator_index in range(num_discriminators):
+			single_discriminator = NLayerDiscriminator(input_channels, num_filters, num_layers)
+			setattr(self, 'discriminator_layer_{}'.format(discriminator_index), single_discriminator.model)
+		self.downsample = nn.AvgPool2d(kernel_size = 3, stride = 2, padding = [ 1, 1 ], count_include_pad = False) # type:ignore[arg-type]
 
-    def single_discriminator_forward(self, model_layers : nn.Sequential, input_tensor : Tensor) -> List[Tensor]:
-        return [ model_layers(input_tensor) ]
+	def single_discriminator_forward(self, model_layers : nn.Sequential, input_tensor : Tensor) -> List[Tensor]:
+		return [ model_layers(input_tensor) ]
 
-    def forward(self, input_tensor : Tensor) -> DiscriminatorOutputs:
-        discriminator_outputs = []
-        downsampled_input = input_tensor
+	def forward(self, input_tensor : Tensor) -> DiscriminatorOutputs:
+		discriminator_outputs = []
+		downsampled_input = input_tensor
 
-        for discriminator_index in range(self.num_discriminators):
-            model_layers = getattr(self, 'discriminator_layer_{}'.format(self.num_discriminators - 1 - discriminator_index))
-            discriminator_outputs.append(self.single_discriminator_forward(model_layers, downsampled_input))
+		for discriminator_index in range(self.num_discriminators):
+			model_layers = getattr(self, 'discriminator_layer_{}'.format(self.num_discriminators - 1 - discriminator_index))
+			discriminator_outputs.append(self.single_discriminator_forward(model_layers, downsampled_input))
 
-            if discriminator_index != (self.num_discriminators - 1):
-                downsampled_input = self.downsample(downsampled_input)
-        return discriminator_outputs
+			if discriminator_index != (self.num_discriminators - 1):
+				downsampled_input = self.downsample(downsampled_input)
+		return discriminator_outputs
