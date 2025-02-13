f05ff6cdb12804ed8cd5281ee965b1baa6c812d3
henryruhs <info@henryruhs.com>
2025-02-13 12:52:59 +0100

    Enforce IR version for older onnxruntime

--- embedding_converter/src/exporting.py
+++ embedding_converter/src/exporting.py
@@ -13,10 +13,12 @@ def export() -> None:
 	directory_path = CONFIG.get('exporting', 'directory_path')
 	source_path = CONFIG.get('exporting', 'source_path')
 	target_path = CONFIG.get('exporting', 'target_path')
+	ir_version = CONFIG.getint('exporting', 'ir_version')
 	opset_version = CONFIG.getint('exporting', 'opset_version')
 
 	makedirs(directory_path, exist_ok = True)
-	embedding_converter_trainer = EmbeddingConverterTrainer.load_from_checkpoint(source_path, map_location = 'cpu')
-	embedding_converter_trainer.eval()
+	model = EmbeddingConverterTrainer.load_from_checkpoint(source_path, map_location = 'cpu')
+	model.eval()
+	model.ir_version = ir_version
 	input_tensor = torch.randn(1, 512)
-	torch.onnx.export(embedding_converter_trainer, input_tensor, target_path, input_names = [ 'input' ], output_names = [ 'output' ], opset_version = opset_version)
+	torch.onnx.export(model, input_tensor, target_path, input_names = [ 'input' ], output_names = [ 'output' ], opset_version = opset_version)
