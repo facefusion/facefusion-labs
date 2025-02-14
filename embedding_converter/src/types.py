650551c19bf554468cf6ab1efa424778f2f8a906
henryruhs <info@henryruhs.com>
2025-02-14 16:19:14 +0100

    Simplify Batch type

--- embedding_converter/src/types.py
+++ embedding_converter/src/types.py
@@ -3,11 +3,10 @@ from typing import Any, Tuple, TypeAlias
 from numpy.typing import NDArray
 from torch import Tensor
 
+Batch : TypeAlias = Tuple[Tensor, Tensor]
+
 Embedding : TypeAlias = NDArray[Any]
 EmbeddingDataset : TypeAlias = NDArray[Embedding]
 FaceLandmark5 : TypeAlias = NDArray[Any]
 
 VisionFrame : TypeAlias = NDArray[Any]
-VisionTensor : TypeAlias = Tensor
-
-Batch : TypeAlias = Tuple[VisionTensor, VisionTensor]
--- face_swapper/src/types.py
+++ face_swapper/src/types.py
@@ -5,6 +5,8 @@ from numpy.typing import NDArray
 from torch import Tensor
 from torch.nn import Module
 
+Batch : TypeAlias = Tuple[Tensor, Tensor, Tensor]
+
 ImagePathList : TypeAlias = List[str]
 ImagePathSet : TypeAlias = Dict[str, ImagePathList]
 
@@ -21,8 +23,6 @@ Padding : TypeAlias = Tuple[int, int, int, int]
 VisionFrame : TypeAlias = NDArray[Any]
 LossTensor : TypeAlias = Tensor
 VisionTensor : TypeAlias = Tensor
-
-Batch : TypeAlias = Tuple[VisionTensor, VisionTensor, Tensor]
 
 GeneratorLossSet : TypeAlias = Dict[str, Tensor]
 DiscriminatorLossSet : TypeAlias = Dict[str, Tensor]
