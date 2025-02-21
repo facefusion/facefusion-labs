3cf9711df0dfe5730767733ebe9ca4179a12ba66
henryruhs <info@henryruhs.com>
2025-02-21 01:09:57 +0100

    Modernize data loader, remove read image helper

--- embedding_converter/src/helper.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-from .types import VisionFrame
-
-
-def read_image(image_path : str) -> VisionFrame:
-	return cv2.imread(image_path)
--- face_swapper/src/data_loader.py
+++ face_swapper/src/data_loader.py
@@ -3,11 +3,11 @@ import os.path
 import random
 from typing import Tuple
 
+import cv2
 import torch
-import torchvision.transforms as transforms
+import transforms from torchvision
 from torch.utils.data import TensorDataset
 
-from .helper import read_image
 from .types import Batch, ImagePathList, ImagePathSet
 
 
@@ -16,11 +16,10 @@ class DataLoaderVGG(TensorDataset):
 		self.same_person_probability = same_person_probability
 		self.directory_paths = glob.glob(dataset_directory_pattern.format(dataset_path))
 		self.image_paths, self.image_path_set = self.prepare_image_paths(dataset_image_pattern)
-		self.dataset_total = len(self.image_paths)
 		self.transforms = self.compose_transforms()
 
 	def __getitem__(self, index : int) -> Batch:
-		source_image_path = self.image_paths[index]
+		source_image_path = self.image_paths.get(index)
 
 		if random.random() > self.same_person_probability:
 			return self.prepare_same_person(source_image_path)
@@ -28,8 +27,9 @@ class DataLoaderVGG(TensorDataset):
 		return self.prepare_different_person(source_image_path)
 
 	def __len__(self) -> int:
-		return self.dataset_total
+		return len(self.image_paths)
 
+	#todo: remove this method - only use glob.glob in init()
 	def prepare_image_paths(self, dataset_image_pattern : str) -> Tuple[ImagePathList, ImagePathSet]:
 		image_paths = []
 		image_path_set = {}
@@ -40,7 +40,7 @@ class DataLoaderVGG(TensorDataset):
 		return image_paths, image_path_set
 
 	def compose_transforms(self) -> transforms:
-		transform = transforms.Compose(
+		return transforms.Compose(
 		[
 			transforms.ToPILImage(),
 			transforms.Resize((256, 256), interpolation = transforms.InterpolationMode.BICUBIC),
@@ -50,22 +50,22 @@ class DataLoaderVGG(TensorDataset):
 			transforms.Lambda(lambda temp_tensor : temp_tensor[[2, 1, 0], :, :]),
 			transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 		])
-		return transform
 
 	def prepare_different_person(self, source_image_path : str) -> Batch:
 		is_same_person = torch.tensor(0)
 		target_image_path = random.choice(self.image_paths)
-		source_vision_frame = read_image(source_image_path)
-		target_vision_frame = read_image(target_image_path)
+		source_vision_frame = cv2.imread(source_image_path)
+		target_vision_frame = cv2.imread(target_image_path)
 		source_tensor = self.transforms(source_vision_frame)
 		target_tensor = self.transforms(target_vision_frame)
 		return source_tensor, target_tensor, is_same_person
 
 	def prepare_same_person(self, source_image_path : str) -> Batch:
 		is_same_person = torch.tensor(1)
+		#todo: why not like in prepare_different_person
 		target_image_path = random.choice(self.image_path_set.get(os.path.dirname(source_image_path)))
-		source_vision_frame = read_image(source_image_path)
-		target_vision_frame = read_image(target_image_path)
+		source_vision_frame = cv2.imread(source_image_path)
+		target_vision_frame = cv2.imread(target_image_path)
 		source_tensor = self.transforms(source_vision_frame)
 		target_tensor = self.transforms(target_vision_frame)
 		return source_tensor, target_tensor, is_same_person
