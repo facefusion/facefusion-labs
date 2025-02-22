ff38fdc3f7d0e9da7252f739987b3cc96bb549c9
henryruhs <info@henryruhs.com>
2025-02-22 10:43:08 +0100

    Fix Generator

--- face_swapper/src/networks/unet.py
+++ face_swapper/src/networks/unet.py
@@ -59,7 +59,7 @@ class UNet(nn.Module):
 
 class UNetPro(UNet):
 	def __init__(self) -> None:
-		super(UNetPro, self).__init__()
+		super(UNet, self).__init__()
 		self.resnet = models.resnet34(pretrained = True)
 		self.down_samples = self.create_down_samples(self)
 		self.up_samples = self.create_up_samples()
