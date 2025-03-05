23210801ab8a80e5fd716088c9ae788ef7bebc40
henryruhs <info@henryruhs.com>
2025-03-05 13:10:14 +0100

    Add basic test for aad and unet

--- face_swapper/tests/test_networks.py
+++ face_swapper/tests/test_networks.py
@@ -1,8 +1,8 @@
 import torch
 import pytest
 
-from ..src.networks.aad import AAD
-from ..src.networks.unet import UNet
+from face_swapper.src.networks.aad import AAD
+from face_swapper.src.networks.unet import UNet
 
 
 @pytest.mark.parametrize('output_size', [ 256 ])
