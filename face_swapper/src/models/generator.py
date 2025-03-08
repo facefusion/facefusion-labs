8f1f002c648b8381c86a987eba9f926a61515173
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-03-08 16:05:07 +0530

    add masknet

--- face_swapper/src/models/generator.py
+++ face_swapper/src/models/generator.py
@@ -1,8 +1,10 @@
 from configparser import ConfigParser
+from typing import Tuple
 
 from torch import Tensor, nn
 
 from ..networks.aad import AAD
+from ..networks.masknet import MaskNet
 from ..networks.unet import UNet
 from ..types import Attributes, Embedding
 
@@ -12,13 +14,16 @@ class Generator(nn.Module):
 		super().__init__()
 		self.encoder = UNet(config_parser)
 		self.generator = AAD(config_parser)
+		self.masker = MaskNet(67, 1, 16)
 		self.encoder.apply(init_weight)
 		self.generator.apply(init_weight)
+		self.masker.apply(init_weight)
 
-	def forward(self, source_embedding : Embedding, target_tensor : Tensor) -> Tensor:
+	def forward(self, source_embedding : Embedding, target_tensor : Tensor) -> Tuple[Tensor, Tensor]:
 		target_attributes = self.get_attributes(target_tensor)
 		output_tensor = self.generator(source_embedding, target_attributes)
-		return output_tensor
+		mask_tensor = self.masker(target_tensor, target_attributes[-1])
+		return output_tensor, mask_tensor
 
 	def get_attributes(self, input_tensor : Tensor) -> Attributes:
 		return self.encoder(input_tensor)
