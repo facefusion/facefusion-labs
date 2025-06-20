d8263eb23b022c5a296015075d42bdd3542c8659
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-06-20 08:24:15 +0530

    remove optimization

--- hyperswap/src/training.py
+++ hyperswap/src/training.py
@@ -14,7 +14,7 @@ from torch.utils.data import ConcatDataset, Dataset, random_split
 from torchdata.stateful_dataloader import StatefulDataLoader
 
 from .dataset import DynamicDataset
-from .helper import apply_noise, calc_embedding, erode_mask, freeze_model_parameters, overlay_mask
+from .helper import apply_noise, calc_embedding, erode_mask, overlay_mask
 from .models.discriminator import Discriminator
 from .models.generator import Generator
 from .models.loss import AdversarialLoss, CycleLoss, DiscriminatorLoss, FeatureLoss, GazeLoss, IdentityLoss, MaskLoss, ReconstructionLoss
@@ -50,11 +50,8 @@ class HyperSwapTrainer(LightningModule):
 		self.loss_embedder = torch.jit.load(self.config_loss_embedder_path, map_location = 'cpu').eval()
 		self.gazer = torch.jit.load(self.config_gazer_path, map_location = 'cpu').eval()
 		self.face_masker = torch.jit.load(self.config_face_masker_path, map_location ='cpu').eval()
-		freeze_model_parameters(self.loss_embedder)
 		self.generator = Generator(config_parser)
 		self.discriminator = Discriminator(config_parser)
-		self.generator = torch.compile(self.generator)
-		self.discriminator = torch.compile(self.discriminator)
 		self.discriminator_loss = DiscriminatorLoss()
 		self.adversarial_loss = AdversarialLoss(config_parser)
 		self.cycle_loss = CycleLoss(config_parser)
@@ -159,7 +156,7 @@ class HyperSwapTrainer(LightningModule):
 		self.untoggle_optimizer(discriminator_optimizer)
 
 		if batch_index % self.config_preview_frequency == 0:
-			self.generate_preview(source_tensor, target_tensor, generator_output_tensor, generator_output_mask)
+			self.generate_preview(source_tensor, target_tensor, generator_output_tensor.detach(), generator_output_mask.detach())
 
 		self.log('generator_loss', generator_loss, prog_bar = True)
 		self.log('discriminator_loss', discriminator_loss, prog_bar = True)
