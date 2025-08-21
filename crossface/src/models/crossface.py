833683b80439fafab5a6f6758933df07cdfe31ff
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-08-21 21:52:19 +0530

    cleanup

--- crossface/src/models/crossface.py
+++ crossface/src/models/crossface.py
@@ -4,45 +4,31 @@ from torch import Tensor, nn
 class CrossFace(nn.Module):
 	def __init__(self) -> None:
 		super().__init__()
-		self.linear_layers = self.create_linear_layers()
-		self.norm_layers = self.create_norm_layers()
-		self.residual_layer = nn.Linear(512, 512)
-		self.gelu = nn.GELU()
-		self.dropout = nn.Dropout(0.1)
+		self.sequence = self.create_sequence()
+		self.linear = nn.Linear(512, 512)
 		self.apply(init_weight)
 
 	@staticmethod
-	def create_linear_layers() -> nn.ModuleList:
-		return nn.ModuleList(
-		[
+	def create_sequence() -> nn.Sequential:
+		return nn.Sequential(
 			nn.Linear(512, 1024),
-			nn.Linear(1024, 2048),
-			nn.Linear(2048, 1024),
-			nn.Linear(1024, 512)
-		])
-
-	@staticmethod
-	def create_norm_layers() -> nn.ModuleList:
-		return nn.ModuleList(
-		[
 			nn.LayerNorm(1024),
+			nn.GELU(),
+			nn.Dropout(0.1),
+			nn.Linear(1024, 2048),
 			nn.LayerNorm(2048),
-			nn.LayerNorm(1024)
-		])
+			nn.GELU(),
+			nn.Dropout(0.1),
+			nn.Linear(2048, 1024),
+			nn.LayerNorm(1024),
+			nn.GELU(),
+			nn.Dropout(0.1),
+			nn.Linear(1024, 512)
+		)
 
 	def forward(self, input_tensor : Tensor) -> Tensor:
 		temp_tensor = nn.functional.normalize(input_tensor, p = 2, dim = -1)
-		residual_tensor = self.residual_layer(temp_tensor)
-
-		for index, layer in enumerate(self.linear_layers[:-1]):
-			temp_tensor = layer(temp_tensor)
-			temp_tensor = self.norm_layers[index](temp_tensor)
-			temp_tensor = self.gelu(temp_tensor)
-			temp_tensor = self.dropout(temp_tensor)
-
-		temp_tensor = self.linear_layers[-1](temp_tensor)
-		temp_tensor = temp_tensor + 0.2 * residual_tensor
-		return temp_tensor
+		return self.sequence(temp_tensor) + 0.2 * self.linear(temp_tensor)
 
 
 def init_weight(module : nn.Module) -> None:
